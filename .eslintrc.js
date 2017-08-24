6ea7d907137be5bad2a137e5cb3540a3c5cca286
Antonio Fontaine <afontaine@uc.cl>
2017-08-24 20:06:10 -0300

    minor fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,7 @@ module.exports = {
     "max-params": [2, 4],
     "max-nested-callbacks": [2, 3],
     "smells/no-switch": 1,
-    "smells/no-complex-switch-case": 1,
+    "smells/no-complex-switch-case": 2,
     "smells/no-setinterval": 1,
     "smells/no-this-assign": 1,
     "jsx-a11y/href-no-hash": 0,
